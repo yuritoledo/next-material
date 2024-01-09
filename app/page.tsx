af4d5090feb925bbd4d5b1b66c5a4941f4564fb6
Yuri Toledo <yuriwtoledo@gmail.com>
2024-01-09 11:04:03 -0300

    adding more mui examples

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,62 @@
-import { Button } from "@mui/material";
+"use client";
+
+import { Button, Snackbar, TextField } from "@mui/material";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const [open, setOpen] = useState(false);
+  const [input, setInput] = useState("");
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const action = (
+    <Button color="secondary" size="small" onClick={handleClose}>
+      UNDO
+    </Button>
+  );
   return (
-    <main style={{ backgroundColor: "white" }}>
-      <Button>Hello World</Button>
+    <main className="bg-blue-100 px-4 h-screen flex flex-col gap-8 items-center justify-center">
+      <Button
+        variant="outlined"
+        className="hover:bg-green-900 hover:text-white hover:border-white"
+      >
+        Hello World
+      </Button>
+
+      <Button onClick={handleClick}>Open simple snackbar</Button>
+
+      <TextField
+        id="outlined-basic"
+        label="Outlined"
+        variant="outlined"
+        error={input.length > 3}
+        helperText={
+          input.length > 3
+            ? "Must be 3 characters or less"
+            : "Fill up until 3 characters"
+        }
+        onChange={(e) => setInput(e.target.value)}
+        value={input}
+        inputProps={{
+          className: "text-blue-500",
+        }}
+      />
+
+      <Snackbar
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        message="Note archived"
+        action={action}
+        className="text-red-500"
+      />
     </main>
   );
 }
